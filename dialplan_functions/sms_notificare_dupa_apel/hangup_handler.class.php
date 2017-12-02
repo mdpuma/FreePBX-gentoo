2700d8165707c3b77350653e2f99e29bf0045203
pumamd <pumamd@yandex.ru>
2017-12-02 21:36:52 +0200

    * fixes

--- dialplan_functions/sms_notificare_dupa_apel/hangup_handler.class.php
+++ dialplan_functions/sms_notificare_dupa_apel/hangup_handler.class.php
@@ -1,81 +1,16 @@
-#!/usr/bin/env php
 <?php
-// Required contexts:
 
-// [cidlookup]
-// exten => cidlookup_1,n,Gosub(predial-hook2,s,1)
-//
-// [predial-hook2]
-// exten => s,1,Set(CHANNEL(hangup_handler_wipe)=hangup-hook2,s,1)
-// same => n,Return()
-// 
-// [hangup-hook2]
-// exten => s,1,System(/var/lib/asterisk/bin/email_reminder_by_csv.php --action=notifynow --src="${CALLERID(num)}" --srcname="${CALLERID(name)}" --did="${CDR(dnid)}" --dst="${CDR(dstchannel)}" --disposition="${DIALSTATUS}" --context="${CDR(dcontext)}" --department="sales" >&1 | tee -a /tmp/asterisk.php.log)
-// same => n,Return()
-
-
-require_once 'PHPMailer/class.phpmailer.php';
-require_once 'PHPMailer/class.smtp.php';
-
-// +------------------------------+//
-
-$config = array(
-    'email' => array(
-        'from' => 'sip@sip.local',
-        'to' => 'sip@sip.local',
-        'use_smtp' => 0,
-        'smtphost' => '',
-        'username' => '',
-        'password' => '',
-    ),
-    'missedcall_file' => '/tmp/apeluri_pierdute.csv',
-    
-    // for email notification about lost calls by individual pair of manager=> email
-    'managers_file' => '/var/lib/asterisk/bin/managers.csv',
-    'telegram' => array(
-         'script' => '/var/lib/asterisk/bin/telegram_bot.php',
-//        'script' => '/var/lib/asterisk/bin/telegram.php',
-        'destination' => 'sales',
-        'departments' => [
-            'sales' => 'sales',
-        ]
-    ),
-    'debug' => 1,
-);
-
-// +------------------------------+//
-
-ini_set('date.timezone', 'Europe/Chisinau');
-ini_set('display_errors', 'On');
-error_reporting(E_ALL & ~E_NOTICE);
-
-$o = getopt('', array(
-    'action:',
-    'src:',
-    'srcname:',
-    'did:',
-    'dst:',
-    'disposition:',
-    'context:',
-    'department:',
-));
-
-// +------------------------------+//
-
-if ($o['action'] == 'store') {
-    // check if this is unanswered call
-    check_is_missing($o);
-
-    $file_exists = 0;
-    if (is_file($config['missedcall_file'])) $file_exists = 1;
-    $fp = fopen($config['missedcall_file'], 'a+');
+function store_missed_call($o, $file) {
+    if (is_file($file)) $file_exists = 1;
+    $fp = fopen($file, 'a+');
     if ($file_exists == 0) fwrite($fp, '"ora-data";"numar client";"numar apelat";"dispozitia"' . "\n");
     fwrite($fp, '"' . date("d.m.Y H:i:s") . '";"' . $o['src'] . '";"' . $o['did'] . '";"' . $o['disposition'] . "\"\n");
     fclose($fp);
 }
-elseif ($o['action'] == 'sendemail') {
+
+function send_missed_call_email($destination, $config, $attachment) {
     $mail = new PHPMailer();
-    if ($EMAIL_SMTP == 1) {
+    if ($config['email']['smtp'] == 1) {
         $mail->isSMTP();
         $mail->Host = $config['email']['smtphost'];
         $mail->Port = 587;
@@ -90,34 +25,16 @@ elseif ($o['action'] == 'sendemail') {
     $mail->From = $config['email']['from'];
     $mail->FromName = 'FreePBX';
     $mail->Subject = 'PBX: apeluri pierdute';
-    $emails = explode(',', $config['email']['to']);
-    foreach ($emails as $i) $mail->AddAddress($i);
+    $mail->AddAddress($destination);
 
-    if (!is_file($config['missedcall_file'])) {
+    if (!is_file($attachment)) {
         $mail->Body = 'Nu sunt inregistrate apeluri pierdute';
     }
     else {
         $mail->Body = 'Gasiti atasament';
-        $mail->AddAttachment($config['missedcall_file'], basename($config['missedcall_file']));
+        $mail->AddAttachment($attachment, basename($attachment));
     }
-
-    if (!$mail->send()) {
-        if($config['debug']==1) debug("Mailer Error: " . $mail->ErrorInfo);
-    }
-    else {
-        if($config['debug']==1) debug("Message sent!");
-        @unlink($config['missedcall_file']);
-    }
-}
-elseif ($o['action'] == 'notifynow') {
-    // check if this is unanswered call
-    check_is_missing($o);
-
-//    send_email();
-    send_telegram_msg(get_telegram_chat($o['department']), get_missedcall_template());
-}
-elseif ($o['action'] == 'force_notify') {
-    send_telegram_msg(get_telegram_chat($o['department']), "Intrare apel pe numarul ${o['did']} de la ${o['src']} (${o['srcname']})");
+    return $mail->send();
 }
 
 function check_is_missing($o) {
@@ -150,7 +67,7 @@ function get_telegram_chat($department=null) {
     if(isset($o['department']) && !empty($o['department']) && isset($config['telegram']['departments'][$o['department']])) {
         return $config['telegram']['departments'][$o['department']].'"';
     } else {
-        return $config['telegram']['destination'];
+        return $config['telegram']['default_destination'];
     }
 }
 
---
+++ dialplan_functions/sms_notificare_dupa_apel/hangup_handler.php
@@ -0,0 +1,116 @@
+#!/usr/bin/env php
+<?php
+// Required contexts:
+
+// [cidlookup]
+// exten => cidlookup_1,n,Gosub(predial-hook2,s,1)
+//
+// [predial-hook2]
+// exten => s,1,Set(CHANNEL(hangup_handler_wipe)=hangup-hook2,s,1)
+// same => n,Return()
+// 
+// [hangup-hook2]
+// exten => s,1,System(/var/lib/asterisk/bin/hangup_handler.php --action=notifynow --src="${CALLERID(num)}" --srcname="${CALLERID(name)}" --did="${CDR(dnid)}" --dst="${CDR(dstchannel)}" --disposition="${DIALSTATUS}" --context="${CDR(dcontext)}" --department="sales" >&1 | tee -a /tmp/asterisk.php.log)
+// same => n,Return()
+
+
+require_once 'PHPMailer/class.phpmailer.php';
+require_once 'PHPMailer/class.smtp.php';
+require_once 'hangup_handler.class.php';
+
+// +------------------------------+//
+
+$config = array(
+    'email' => array(
+        'from' => 'sip@sip.local',
+        'use_smtp' => 0,
+        'smtphost' => '',
+        'username' => '',
+        'password' => '',
+        'default_destination' => 'sip@sip.local',
+        'departments' => [
+            'sales-worktime' => 'rodica@euroterm.md,operator@euroterm.md',
+            'sales' => 'rodica@euroterm.md',
+            'service-worktime' => 'service@euroterm.md,service1@euroterm.md',
+            'service' => 'service1@euroterm.md',
+            'tech' => 'inginer2@euroterm.md,marcinschi@euroterm.md',
+        ]
+    ),
+    
+    // for email notification about lost calls by individual pair of manager=> email
+    'managers_file' => '/var/lib/asterisk/bin/managers.csv',
+    'telegram' => array(
+        'script' => '/var/lib/asterisk/bin/telegram_bot.php',
+        'default_destination' => 'Euroterm',
+        'departments' => [
+            'sales' => 'Euroterm',
+            'service' => 'Euroterm Service',
+            'tech' => 'Euroterm Tehnic'
+        ]
+    ),
+    'debug' => 1,
+);
+
+// +------------------------------+//
+
+ini_set('date.timezone', 'Europe/Chisinau');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL & ~E_NOTICE);
+
+$o = getopt('', array(
+    'action:',
+    'src:',
+    'srcname:',
+    'did:',
+    'dst:',
+    'disposition:',
+    'context:',
+    'department:',
+));
+
+// +------------------------------+//
+
+if ($o['action'] == 'store') {
+    // check if this is unanswered call
+    check_is_missing($o);
+
+    if(isset($o['department']) && $o['department']!='') {
+        $missedcall_file = '/tmp/missedcall_'.$o['department'].'.csv';
+    } else {
+        $missedcall_file = '/tmp/missedcall.csv';
+    }
+    store_missed_call($o, $missedcall_file);
+}
+elseif ($o['action'] == 'sendemail') {
+    foreach($config['email']['departments'] as $department => $destination) {
+        $missedcall_file = '/tmp/missedcall_'.$department.'.csv';
+        $list = explode(",", $destination);
+        foreach($list as $email) {
+            $return = send_missed_call_email($email, $config, $missedcall_file);
+            if(!$return) debug("Mailer Error: " . $mail->ErrorInfo);
+        }
+        @unlink($missedcall_file);
+    }
+    
+    // send missedcall list for default_destination if exists
+    $missedcall_file = '/tmp/missedcall.csv';
+    if(is_file($missedcall_file)) {
+        $return = send_missed_call_email($config['email']['default_destination'], $config, $missedcall_file);
+        if(!$return) debug("Mailer Error: " . $mail->ErrorInfo);
+        else @unlink($missedcall_file); 
+    }
+}
+elseif ($o['action'] == 'notifynow') {
+    // check if this is unanswered call
+    check_is_missing($o);
+
+//    send_email();
+    send_telegram_msg(get_telegram_chat($o['department']), get_missedcall_template());
+}
+elseif ($o['action'] == 'force_notify') {
+    send_telegram_msg(get_telegram_chat($o['department']), "Intrare apel pe numarul ${o['did']} de la ${o['src']} (${o['srcname']})");
+}
+
+// +------------------------------+//
+
+?>
