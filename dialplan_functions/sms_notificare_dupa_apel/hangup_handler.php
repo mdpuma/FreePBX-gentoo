f7699ef78b592e52203ed79e068a6258cbee8d37
pumamd <pumamd@yandex.ru>
2018-02-05 21:35:14 +0200

    * simplified selectare_manager context, using Return to standard freepbx dialplan * fixes on transfer-context * some cleans

--- dialplan_functions/sms_notificare_dupa_apel/hangup_handler.php
+++ dialplan_functions/sms_notificare_dupa_apel/hangup_handler.php
@@ -2,15 +2,19 @@
 <?php
 // Required contexts:
 
-// [cidlookup]
-// exten => cidlookup_1,n,Gosub(hangup-hook1,s,1)
-//
-// [hangup-hook1]
-// exten => install,1,Set(CHANNEL(hangup_handler_wipe)=hangup-hook1,run,1)
-// same => n,Return()
-// exten => run,1,System(/var/lib/asterisk/bin/hangup_handler.php --action=notifynow --src="${CALLERID(num)}" --srcname="${CALLERID(name)}" --did="${CDR(dnid)}" --dst="${CDR(dstchannel)}" --disposition="${DIALSTATUS}")
-// ; use disposition="${CDR(disposition)}" if call may end on queue
-// same => n,Return()
+/*
+
+[cidlookup]
+exten => cidlookup_1,n,Gosub(hangup-hook1,install,1)
+
+[hangup-hook1]
+exten => install,1,Set(CHANNEL(hangup_handler_wipe)=hangup-hook1,run,1)
+same => n,Return()
+exten => run,1,System(/var/lib/asterisk/bin/hangup_handler.php --action=notifynow --src="${CALLERID(num)}" --srcname="${CALLERID(name)}" --did="${CDR(dnid)}" --dst="${CDR(dstchannel)}" --disposition="${DIALSTATUS}")
+; use disposition="${CDR(disposition)}" if call may end on queue
+same => n,Return()
+
+*/
 
 
 require_once 'PHPMailer/class.phpmailer.php';
