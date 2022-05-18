11df2caa34efb752ca270b70461466fc122ca8f1
puma <admin@iphost.md>
2022-05-18 07:12:42 +0300

    * update

--- scripts/restart_asterisk.sh
+++ scripts/restart_asterisk.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 /etc/init.d/asterisk restart
 if [ $? -ne 0 ]; then
-        killall asterisk -9 -v
-        /etc/init.d/asterisk restart
-fi 
+	killall asterisk -9 -v
+	/etc/init.d/asterisk zap
+	/etc/init.d/asterisk start
+fi
